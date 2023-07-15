c8bb5a52426326485f9697fed42785bf0eb875c6
Thu Dang <>
2023-07-14 20:11:43 -0500

    update image url

--- src/constants/index.js
+++ src/constants/index.js
@@ -30,27 +30,27 @@ export const navLinks = [
 export const socialMedia = [
   {
     id: "social-media-1",
-    icon: { facebook },
+    icon:  facebook ,
     link: "https://www.facebook.com/",
   },
   {
     id: "social-media-2",
-    icon: { youtube },
+    icon: youtube ,
     link: "https://www.youtube.com/",
   },
   {
     id: "social-media-3",
-    icon: { twitter },
+    icon: twitter ,
     link: "https://www.twitter.com/",
   },
   {
     id: "social-media-4",
-    icon: { pinterest },
+    icon: pinterest ,
     link: "https://www.pinterest.com/",
   },
   {
     id: "social-media-5",
-    icon: { instagram },
+    icon:  instagram ,
     link: "https://www.instagram.com/",
   },
 ];
